5167ba5e2ec143b68bc8d46649617cdfc69bbda4
andgeral <geraldinebecerra93@gmail.com>
2024-02-26 01:21:41 -0300

    se agregan funcionalidad del grafico y ocultar y mostrar algunos elementos

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,27 @@
 $(document).ready(function () {
-  $("#heroButton").click(function () {
+  $(".ocultar").hide();
+  $("#return").hide();
+  $("#return").click(function () {
+    $(".ocultar").hide();
+    $(".prueba").show();
+    $("#heroNumber").val("");
+    $("#return").hide();
+    $("#chartContainer").hide();
+  });
+  $("#heroButton").click(function (e) {
+    $(".ocultar").show();
+    $(".prueba").hide();
+    $("#return").show();
+    $("#chartContainer").show();
+
     //guardamos el número que ingresa el usuario para luego agregarloa a la url
     const heroNumber = $("#heroNumber").val();
+    const regex = /^[0-9]*$/;
+    if (regex.test(heroNumber) === false || heroNumber === "") {
+      e.preventDefault();
+      alert("debes ingresar un número");
+      return;
+    }
     $.ajax({
       type: "GET",
       url:
@@ -9,10 +29,52 @@ $(document).ready(function () {
       dataType: "json",
       success: function (data) {
         console.log(data);
+        let dataPoints = [];
+        let options = {
+          title: {
+            text: `Estadísticas de poder para ${data.name}`,
+          },
+          data: [
+            {
+              type: "pie",
+              startAngle: 45,
+              showInLegend: "true",
+              legendText: "{label}",
+              indexLabel: "{label} ({y})",
+              yValueFormatString: "#,##0.#" % "",
+              dataPoints: dataPoints,
+            },
+          ],
+        };
+        for (const powers in data.powerstats) {
+          dataPoints.push({
+            label: powers,
+            y: data.powerstats[powers],
+          });
+        }
+        $("#chartContainer").CanvasJSChart(options);
+        // Se pinta la información en el html
+        $("#heroImage")
+          .empty()
+          .append(
+            `<img src='${data.image.url}' style=" max-width:200px"; "/>  `
+          );
+        $("#name").text(`Nombre: ${data.name}`);
+        $("#connection").text(
+          `Conexiones: ${data.connections["group-affiliation"]}`
+        );
+        $("#published").text(`Publicado por: ${data.biography.publisher}`);
+        $("#occupation").text(`Ocupación: ${data.work.occupation}`);
+        $("#firstAppearance").text(
+          `Primera Aparición: ${data.biography["first-appearance"]}`
+        );
+        $("#heroHeight").text(`Altura: ${data.appearance.height}`);
+        $("#heroWeight").text(`Peso: ${data.appearance.weight}`);
+        $("#alliances").text(`Alianzas: ${data.biography.aliases}`);
       },
+
       error: function (error) {
-        console.log("error:", error.status);
-        alert("ha ocurrido un error con la pagina: error " + error.status);
+        console.log("error:", error.error);
       },
     });
   });
